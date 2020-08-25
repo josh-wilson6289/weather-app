823c416a162be2f6dc5f2b272afee5105c7e609d
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-25 14:47:51 -0500

    fixed jquery script to include ajax.  got open weather map api working

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # weather-app
 
-API Key: ebbe9aba6bb9ec7a3858466e6dae8ae4
+
--- script.js
+++ script.js
@@ -2,18 +2,12 @@ $(document).ready(function() {
   
   var currentDay = moment().format('LL');
 
-// $.ajax ({
-//     url: "api.openweathermap.org/data/2.5/forecast?q={Austin, Texas}&appid={ebbe9aba6bb9ec7a3858466e6dae8ae4}",
-//     method: "GET"
-//   }).then (function(response) {
-//     console.log(response);
-//   });
-
-  $.ajax({
-    url: "https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy",
+$.ajax ({
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=Austin, Texas&appid=ebbe9aba6bb9ec7a3858466e6dae8ae4",
     method: "GET"
-  }).then(function(response) {
+  }).then (function(response) {
     console.log(response);
   });
+
 
 });
