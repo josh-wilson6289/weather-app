ef00aadbfdfa007546842d1c3a38ea638e909d29
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-25 14:38:09 -0500

    trying to get ajax to work

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-var currentDay = moment().format('LL');
+$(document).ready(function() {
+  
+  var currentDay = moment().format('LL');
 
+// $.ajax ({
+//     url: "api.openweathermap.org/data/2.5/forecast?q={Austin, Texas}&appid={ebbe9aba6bb9ec7a3858466e6dae8ae4}",
+//     method: "GET"
+//   }).then (function(response) {
+//     console.log(response);
+//   });
+
+  $.ajax({
+    url: "https://www.omdbapi.com/?t=romancing+the+stone&y=&plot=short&apikey=trilogy",
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+  });
+
+});
