40cdce85b40abc430e3ed477f687460424b712c3
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-26 11:54:26 -0500

    submit button to run getCurrentWeather function

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
-  
-  var currentDay = moment().format('LL');
+ 
+var city = "";
 
-$.ajax ({
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=Austin, Texas&appid=ebbe9aba6bb9ec7a3858466e6dae8ae4",
+function getCurrentWeather(){
+  var querySelector = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=ebbe9aba6bb9ec7a3858466e6dae8ae4"
+
+  $.ajax ({
+    url: querySelector,
     method: "GET"
   }).then (function(response) {
     console.log(response);
   });
+}
+
+// getCurrentWeather();
+//   function tempConversion(){
+
+//   }
+
+$("#citySubmitBtn").click(function () {
+   event.preventDefault();
+   city = $("#citySearch").val();
+   getCurrentWeather(city);
+});
 
 
 });
+
