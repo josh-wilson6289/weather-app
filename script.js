dca258a86bd9b46a8025346e4d42424f33efbf61
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-26 13:06:30 -0500

    got city, temp, humidity, and windspeed into card.  Need to switch API to one call in order to get UV index and get daily forecasts

--- script.js
+++ script.js
@@ -1,27 +1,48 @@
 $(document).ready(function() {
  
-var city = "";
+var citySearch = "";
+var currentCity = $("#currentCity");
+var currentTemp = $("#currentTemp");
+var currentHumidity = $("#currentHumidity");
+var currentWindSpeed = $("#currentWindSpeed");
+var currentUvIndex = $("#currentUvIndex");
+var fTemp;
+
+function tempConversion(kTemp) {
+  fTemp = ((kTemp-273.15)*1.8)+32;
+  fTemp = Math.floor(fTemp);
+  return fTemp;
+}
 
 function getCurrentWeather(){
-  var querySelector = "https://api.openweathermap.org/data/2.5/forecast?q=" + city +"&appid=ebbe9aba6bb9ec7a3858466e6dae8ae4"
+  var querySelector = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearch +"&appid=ebbe9aba6bb9ec7a3858466e6dae8ae4"
 
   $.ajax ({
     url: querySelector,
     method: "GET"
   }).then (function(response) {
+    //converts kalvin to farenheight
     console.log(response);
+    
+    tempConversion(response.list[0].main.temp);
+
+    currentCity.text(response.city.name + " Weather");
+    currentTemp.text(fTemp + " Degrees");
+    currentHumidity.text(response.list[0].main.humidity);
+    currentWindSpeed.text(response.list[0].wind.speed);
+    
+    
+    
   });
 }
 
-// getCurrentWeather();
-//   function tempConversion(){
 
-//   }
 
+// click event to run getCurrentWeather function when search button is clicked.
 $("#citySubmitBtn").click(function () {
    event.preventDefault();
-   city = $("#citySearch").val();
-   getCurrentWeather(city);
+   citySearch = $("#citySearch").val();
+   getCurrentWeather(citySearch);
 });
 
 
